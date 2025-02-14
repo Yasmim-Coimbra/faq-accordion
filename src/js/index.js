b76f24eef91a2d42d787f226c1090d643309f080
Yasmim Coimbra <ymdsc.coimbra85@gmail.com>
2025-02-13 23:23:03 -0300

    JS: adding functionality: using 'forEach' and 'click' methods to toggle the answers and its icons

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,11 @@ const questions = document.querySelectorAll(".clickable");
 const answers = document.querySelectorAll(".answer");
 const plusIcon = document.querySelectorAll(".plus-icon");
 const minusIcon = document.querySelectorAll(".minus-icon");
+
+questions.forEach((question, indexPergunta) => {
+    question.addEventListener("click", () => {
+        answers[indexPergunta].classList.toggle("active");
+        plusIcon[indexPergunta].classList.toggle("on");
+        minusIcon[indexPergunta].classList.toggle("on");
+    })
+})
