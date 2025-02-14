916620b67d473cdf3f53a6b45c560f02b366b029
Yasmim Coimbra <ymdsc.coimbra85@gmail.com>
2025-02-13 23:18:40 -0300

    JS: identifying the DOM elements i am going to manipulate

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+const questions = document.querySelectorAll(".clickable");
+const answers = document.querySelectorAll(".answer");
+const plusIcon = document.querySelectorAll(".plus-icon");
+const minusIcon = document.querySelectorAll(".minus-icon");
